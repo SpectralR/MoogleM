11708bb8b734dc12dba7eea95483808f2052992a
SpectralR <spectralr@outlook.fr>
2019-10-19 09:42:21 +0200

    started to work on notifications for the forum

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use App\Topic;
 use App\Message;
 use App\Category;
 use App\Forms\TopicForm;
 use App\Forms\MessageForm;
 use Illuminate\Http\Request;
+use App\Notifications\ForumNewMsg;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Kris\LaravelFormBuilder\FormBuilder;
 use App\Repositories\MessageRepoInterface;
+use Illuminate\Support\Facades\Notification;
 use Kris\LaravelFormBuilder\FormBuilderTrait;
 
 class ForumController extends Controller
@@ -60,11 +63,15 @@ class ForumController extends Controller
     {
         $form = $this->form(MessageForm::class);
         $user = Auth::user();
+        $users = User::get();
         $topicObj = Topic::find($topic)->get(); 
-        // dd($topicObj); 
+        foreach ($users as $key => $value) {
+            if ($value->name === $user->name) {
+                $users->forget($key);
+            }
+        }
 
         if($form->isValid()) {
-            //  dd($user->id);   
             $message = new Message([
                 'message' => $request->wysiwyg,
                 'topic_id' => $topic
@@ -72,6 +79,8 @@ class ForumController extends Controller
 
             $user->messages()->save($message);
             $message->save();
+
+            Notification::send($users, new ForumNewMsg($message));
 
             return redirect()->route('forum_messages',[
                 'topic' => $topic
--- app/Notifications/ForumNewMsg.php
+++ app/Notifications/ForumNewMsg.php
@@ -29,21 +29,7 @@ class ForumNewMsg extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail'];
-    }
-
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+        return ['database'];
     }
 
     /**
@@ -55,7 +41,7 @@ class ForumNewMsg extends Notification
     public function toArray($notifiable)
     {
         return [
-            //
+            
         ];
     }
 }
