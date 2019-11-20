f9198143e29c4128309c4deac717ac3d55f37af2
SpectralR <spectralya@vps596466.ovh.net>
2019-11-20 14:45:44 +0100

    bug fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,7 +109,6 @@
     </footer>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="{{ asset('Trumbowyg/dist/trumbowyg.min.js') }}"></script>
 <script src="{{ asset('js/wysiwyg.js') }}"></script>
 <script src="{{ asset('slick/slick.min.js') }}"></script>
